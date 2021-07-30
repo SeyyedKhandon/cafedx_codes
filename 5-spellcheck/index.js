7191ececb1d1defd1dcfcee4aa65cc584f8e207f
SeyyedKhandon <SeyyedKhandon@gmail.com>
2021-07-30 18:16:20 +0430

    feat: add 5-spell cheker

---
+++ 5-spellcheck/index.js
@@ -0,0 +1,14 @@
+const numberOfUsrs = 10;
+
+function getUserInfoo() {
+  return { name: "SeyyedMahdi", famly: "Hassanpour" };
+}
+
+function deletUser(user) {
+  localStorag.removeItem(user);
+}
+
+// this is coment
+function loger(txt) {
+  console.log("SMHP is my own specal word");
+}
